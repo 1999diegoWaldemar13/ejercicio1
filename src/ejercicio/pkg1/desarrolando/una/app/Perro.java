8a6b569f4958e77149a25c415f6e55ff5ae5ebfc
diego <diego@diego-CX-Nice-Evo-II>
2023-08-08 19:31:07 -0300

    primercomit

---
+++ src/ejercicio/pkg1/desarrolando/una/app/Perro.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg1.desarrolando.una.app;
+
+/**
+ *
+ * @author diego
+ */
+public class Perro {
+    public int años;
+    
+}
